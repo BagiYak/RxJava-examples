4644698bb4025c27deea7eb69aa9ce8316f518ad
Bagdat <68787724+BagiYak@users.noreply.github.com>
2022-01-29 01:35:14 +0600

    Disposables
    Lecture -->> https://codingwithmitch.com/courses/rxjava-rxandroid-for-beginners/disposables/

--- app/src/main/java/kz/bagiyak/rxjava_examples/MainActivity.kt
+++ app/src/main/java/kz/bagiyak/rxjava_examples/MainActivity.kt
@@ -9,6 +9,7 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.schedulers.Schedulers
 import android.widget.TextView
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +18,9 @@ class MainActivity : ComponentActivity() {
 
     //ui
     private var text: TextView? = null
+
+    //vars
+    private val disposables = CompositeDisposable() // Instantiate a new CompositeDisposable object. Typically this will be a global variable inside your Activity or ViewModel.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,19 +44,41 @@ class MainActivity : ComponentActivity() {
 
         taskObservable.subscribe(object : Observer<Task> {
 
-            override fun onSubscribe(d: Disposable?) {}
+            override fun onSubscribe(d: Disposable?) {
+                Log.d(TAG, "onSubscribe: Thread name:: -> " + Thread.currentThread().name)
+                Log.d(TAG, "onSubscribe: called.")
+                disposables.add(d)  // Add the disposable to the CompositeDisposable.
+            }
 
             override fun onNext(t: Task) {
                 Log.d(TAG, "onNext: Thread name:: -> " + Thread.currentThread().name)
                 Log.d(TAG, "onNext: task description:: -> " + t.description)
             }
 
-            override fun onError(e: Throwable?) {}
+            override fun onError(e: Throwable?) {
+                Log.d(TAG, "onError: Thread name:: -> " + Thread.currentThread().name)
+                Log.d(TAG, "onError: ", e)
+            }
 
-            override fun onComplete() {}
+            override fun onComplete() {
+                Log.d(TAG, "onComplete: Thread name:: -> " + Thread.currentThread().name)
+                Log.d(TAG, "onComplete: called.")
+            }
 
         })
 
     }
 
+    // to see calling method disposables.clear() in onDestroy()
+    // just turn your device screen from vertical to horizontal
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy: Thread name:: -> " + Thread.currentThread().name)
+        // Then when the Observer is no longer needed, clear the disposables.
+        // A good place to do this is in the onDestroy() method of an Activity or Fragment.
+        // Or in the onCleared() method of a ViewModel.
+        disposables.clear()
+        Log.d(TAG, "onDestroy: disposables.clear() called")
+    }
 }
